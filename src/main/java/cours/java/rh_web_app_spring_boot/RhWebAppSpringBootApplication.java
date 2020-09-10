d6cf4fb3d301a0374a214444e91da2c3a7aef6bd
OusmaneMballo <ousmanemballo576@gmail.com>
2020-09-10 03:57:03 +0000

    Configuration et connexion a la base de donnee

--- src/main/java/cours/java/rh_web_app_spring_boot/RhWebAppSpringBootApplication.java
+++ src/main/java/cours/java/rh_web_app_spring_boot/RhWebAppSpringBootApplication.java
@@ -1,5 +1,8 @@
 package cours.java.rh_web_app_spring_boot;
 
+import cours.java.rh_web_app_spring_boot.DAO.SpecialiteRepository;
+import cours.java.rh_web_app_spring_boot.model.Specialite;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RhWebAppSpringBootApplication implements CommandLineRunner {
 
+    @Autowired
+    private SpecialiteRepository specialiteRepo;
     public static void main(String[] args) {
         SpringApplication.run(RhWebAppSpringBootApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        Specialite specialite=new Specialite();
+        specialite.setLibelle("Specialite 1");
+        specialiteRepo.save(specialite);
     }
 }
