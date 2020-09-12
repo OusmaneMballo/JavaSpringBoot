34a0b523867510e01c4fe7baeff9cb1662cc024b
OusmaneMballo <ousmanemballo576@gmail.com>
2020-09-12 03:06:03 +0000

    Listing service

--- src/main/java/cours/java/rh_web_app_spring_boot/Controller/MedecinController.java
+++ src/main/java/cours/java/rh_web_app_spring_boot/Controller/MedecinController.java
@@ -1,5 +1,6 @@
 package cours.java.rh_web_app_spring_boot.Controller;
 import cours.java.rh_web_app_spring_boot.DAO.MedecinRepository;
+import cours.java.rh_web_app_spring_boot.DAO.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +13,13 @@ public class MedecinController {
 
     @Autowired
     private MedecinRepository medeciRepos;
+    @Autowired
+    private ServiceRepository serviceRepo;
 
     @GetMapping("/all")
     public String medecinPage(Model model) {
 
+        model.addAttribute("services", serviceRepo.findAll());
         model.addAttribute("medecins",medeciRepos.findAll());
         return "medecin";
     }
--- src/main/java/cours/java/rh_web_app_spring_boot/DAO/ServiceRepository.java
+++ src/main/java/cours/java/rh_web_app_spring_boot/DAO/ServiceRepository.java
@@ -3,7 +3,7 @@ import cours.java.rh_web_app_spring_boot.model.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ServiceRepository extends JpaRepository<Service, Integer>{
 
 }
